d26aed4d214c71b6ace76f2277a8bc4992e67703
fganga <fganga@unaj.edu.ar>
2024-03-07 23:19:05 -0300

    running autobahn with riot.

--- test/autobahn/server.ml
+++ test/autobahn/server.ml
@@ -1,4 +1,3 @@
-open Riot
 
 module Echo_server = struct
   type args = unit
@@ -9,7 +8,7 @@ module Echo_server = struct
       `ok 1
 
   let handle_frame frame _conn _state : (state, [> `Unknown_opcode of int]) Trail.Sock.handle_result =
-    Logger.info (fun f -> f "handling frame: %a" Trail.Frame.pp frame);
+    Riot.Logger.info (fun f -> f "handling frame: %a" Trail.Frame.pp frame);
     `push ([ frame ], _state)
 
   (* val handle_message : *)
@@ -18,9 +17,9 @@ module Echo_server = struct
       `ok 2
 end
 
-module Test : Application.Intf = struct
-
+module Test : Riot.Application.Intf = struct
   let start () =
+    let open Riot in
     Logger.set_log_level (Some Debug);
     sleep 0.1;
     Logger.info (fun f -> f "starting nomad server");
@@ -36,4 +35,81 @@ module Test : Application.Intf = struct
     Nomad.start_link ~port:2112 ~handler ()
 end
 
-let () = Riot.start ~apps:[ (module Logger); (module Test) ] ()
+module Autobahn  = struct
+
+    let get_cwd () =
+        try
+            Unix.getcwd ()
+        with
+        | Unix.Unix_error (err, _, _) ->
+                Printf.eprintf "Could not get current directory: %s\n" (Unix.error_message err);
+                exit 1
+
+
+
+    let launch server_pid () = 
+        Riot.link server_pid;
+
+        let open Unix in
+        
+        let cwd = get_cwd () in
+        
+        let config_volume = Filename.concat cwd "/test/autobahn/fuzzingclient.json:/fuzzingclient.json" in
+        let reports_volume = Filename.concat cwd "/_build/reports:/reports" in
+        
+        let cmd = "docker" in
+        let args = [|
+            "docker";
+            "run";
+			 "--rm";
+			 "-v";
+			 config_volume;
+			 "-v";
+			 reports_volume;
+			 "--name";
+			 "nomad";
+			 "--net=host";
+			 "crossbario/autobahn-testsuite";
+			 "wstest";
+			 "--mode";
+			 "fuzzingclient";
+			 "-w";
+			 "ws://0.0.0.0:2112"
+        |] in
+        
+        let pid = Unix.create_process cmd args stdin stdout stderr in
+        
+        let _, status = Unix.waitpid [] pid in
+        
+        match status with
+        | _ -> 
+                (* the docker image always exits with 0, so if exits stop the server *)
+                Riot.Logger.info (fun f -> f "autobahn exited");
+                Riot.(exit server_pid Normal);
+                ()
+
+
+    let start server_pid =
+        let open Riot in
+        let pid = spawn @@ launch server_pid in
+        Ok pid
+end
+
+let () = Riot.run @@ fun () ->
+    let (let*) = Result.bind in
+
+    let launch () =
+        let* logger_pid = Riot.Logger.start () in
+        let* server_pid = Test.start () in
+        (* if i dont wait a little bit autobahn gets weird and hangs forever *)
+        Unix.sleep 2;
+        let* autobahn_pid = Autobahn.start server_pid in
+        Riot.Logger.info (fun f -> f "started autobahn pid: %a" Riot.Pid.pp autobahn_pid);
+
+        Riot.wait_pids [ logger_pid; server_pid; autobahn_pid ];
+
+        Ok () in
+
+    match launch () with
+    | Ok () -> ()
+    | Error _ -> Riot.shutdown ()
